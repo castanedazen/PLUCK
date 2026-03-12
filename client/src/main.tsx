791264f089f02079f8cbf07a434462f89e68c406
castanedazen <149535722+castanedazen@users.noreply.github.com>
2026-03-12 07:09:56 +0000

    Restore PLUCK base styling and config files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import 'leaflet/dist/leaflet.css';
-import './styles.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
